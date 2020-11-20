b13d32aa7a81bc2ae31fcc08bc53817a0aa9be7d
andreea <andreea.mutu3@gmail.com>
2020-11-20 16:05:48 +0200

    categories

--- budget/Expense.py
+++ budget/Expense.py
@@ -8,6 +8,9 @@ class Expense():
         self.category = category
         self.amount = amount
 
+    def __str__(self):
+        return self.category + " " + self.amount + " "+ self.date_time
+
 
 class Expenses():
     def __init__(self):
@@ -40,3 +43,17 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses={x for x in self.list
+                            if x.category=='Phone' or x.category=='Auto and Gas' or
+                            x.category=='Classes' or x.category=='Utilities' or
+                            x.category=='Mortgage'}
+        food_expenses={x for x in self.list
+                       if x.category=='Groceries' or
+                       x.category=='Eating Out'}
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+        
+        
+        
